542c4aade024829e0b9e16a753f4793d5508d22f
seshanthS <seshanth@protonmail.com>
2022-10-22 18:00:02 +0530

    Add router address

--- parameters/index.js
+++ parameters/index.js
@@ -1,26 +1,26 @@
 module.exports = {
     ethereum: { //ETHEREUM
-        routerAddr: "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F",
+        routerAddr: "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F", //sushiswap
         trustedForwarder: "0x84a0856b038eaAd1cC7E297cF34A7e72685A8693"
     },
     bnb: { //BSC
-        routerAddr: "",
+        routerAddr: "0x10ED43C718714eb63d5aA57B78B54704E256024E", //pancakeswap
         trustedForwarder: "0x86C80a8aa58e0A4fa09A69624c31Ab2a6CAD56b8"
     },
     polygon: { //POLYGON
-        routerAddr: "",
+        routerAddr: "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff", //quickswap
         trustedForwarder: "0xf0511f123164602042ab2bCF02111fA5D3Fe97CD"
     },
     avalanche: { //AVALANCHE
-        routerAddr: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+        routerAddr: "0xE54Ca86531e17Ef3616d22Ca28b0D458b6C89106", //pangolin
         trustedForwarder: "0x64CD353384109423a966dCd3Aa30D884C9b2E057"
     },
     arbitrum: { //ARBITRUM
-        routerAddr: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+        routerAddr: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", //sushiswap
         trustedForwarder: "0xfe0fa3C06d03bDC7fb49c892BbB39113B534fB57"
     },
     goerli: { //GOERLI
-        routerAddr: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+        routerAddr: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", //sushiswap
         trustedForwarder: "0xE041608922d06a4F26C0d4c27d8bCD01daf1f792"
     }
 }
