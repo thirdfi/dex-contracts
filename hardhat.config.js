b03794f8177feca5733a7a8bf8a28ede989ac6fe
Nishith Patnaik <nishithpatnaik7@gmail.com>
2023-02-21 17:25:36 +0530

    added kucoin network configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -45,7 +45,7 @@ module.exports = {
       accounts: [`0x${process.env.PRIVATE_KEY}`]
     },
 
-    "arbitrum-goerli" : {
+    "arbitrum-goerli": {
       url: process.env.RPC_ARBITRUM_GOERLI,
       accounts: [`0x${process.env.PRIVATE_KEY}`]
     },
@@ -69,6 +69,14 @@ module.exports = {
       url: process.env.RPC_OPTIMISM,
       accounts: [`0x${process.env.PRIVATE_KEY}`]
     },
+    kcc: {
+      url: process.env.RPC_KCC,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    "kcc-testnet": {
+      url: process.env.RPC_KCC_TESTNET,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
   },
 
   solidity: "0.8.13",
@@ -90,7 +98,9 @@ module.exports = {
       "arbitrum-goerli": process.env.ARBISCAN_KEY,
       avalancheFujiTestnet: process.env.AVAX_KEY,
       goerli: process.env.ETHERSCAN_KEY,
-      optimisticEthereum: process.env.OPTIMISM_KEY
+      optimisticEthereum: process.env.OPTIMISM_KEY,
+      "kcc-testnet": process.env.KCCSCAN_KEY,
+      kcc: process.env.KCCSCAN_KEY
     },
     customChains: [
       {
@@ -99,6 +109,14 @@ module.exports = {
         urls: {
           apiURL: "https://api-goerli.arbiscan.io/api",
           browserURL: "https://goerli.arbiscan.io"
+        }
+      },
+      {
+        network: "kcc-testnet",
+        chainId: 322,
+        urls: {
+          apiURL: "https://scan-testnet.kcc.network/api",
+          browserURL: "https://scan-testnet.kcc.network"
         }
       }
     ]
